0412f25a9ec710e223e85bf0d058c9e25469050a
Matias Arriola <matias.arriola@gmail.com>
2017-11-12 20:58:37 -0300

    remove service worker

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './theme.css'
-import registerServiceWorker from './registerServiceWorker';
 import Routes from './routes';
 
 const render = Component => ReactDOM.render(<Component />, document.getElementById('root'));
 
 render(Routes);
-registerServiceWorker();
 
 if (module.hot) {
   module.hot.accept('./routes', () => {
